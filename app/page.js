e7e0d8950b74ef758f9d9de7cc7d84096356418d
v.kiriiak <valentyn.kiriiak@playtech.com>
2024-10-16 16:17:26 +0300

    init 8

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+        <Image
+            src="images/logo.svg"
+            width="300"
+            height="300"
+        />
         test8
     </div>
   );
