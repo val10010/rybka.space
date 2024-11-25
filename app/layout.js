55c585ce833355f7bd562f6c9c732eb0c6b2d3f2
v.kiriiak <valentyn.kiriiak@playtech.com>
2024-11-25 15:45:15 +0200

    popup

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,39 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-
+import Script from 'next/script';
 import "./globals.scss";
 import styles from "./page.module.scss";
 
 export const metadata = {
-  title: "rybka.space",
-  description: "твій особистий space краси та комфорту",
+    title: "rybka.space",
+    description: "твій особистий space краси та комфорту",
 };
 
-
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <link rel="icon" type="image/svg+xml" href="/images/favicon.png" />
-    <body>
+    return (
+        <html lang="en">
+        <head>
+            <link rel="icon" type="image/svg+xml" href="/images/favicon.png" />
+        </head>
+        <body>
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-EKSSB3ELFW"
+            strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EKSSB3ELFW');
+          `}
+        </Script>
         <Header/>
         <main className={styles.main}>
             {children}
         </main>
         <Footer/>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
