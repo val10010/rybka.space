c7c2103d0c5631600e3cece397feb1ad3d7d9c25
v.kiriiak <valentyn.kiriiak@playtech.com>
2024-11-25 16:19:05 +0200

    popup

--- app/services/api.js
+++ app/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Default instance
 export const instance = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'rybka-space.vercel.app',
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '/',
     responseType: 'json',
     headers: {
         'Content-Type': 'application/json'
