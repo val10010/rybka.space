14ed6d68aff55c5bc446fc1b478095b18f151b3d
v.kiriiak <valentyn.kiriiak@playtech.com>
2024-12-27 15:58:30 +0200

    seo

--- app/page/components/products/components/product/index.js
+++ app/page/components/products/components/product/index.js
@@ -26,6 +26,8 @@ export default function ProductSlider({ data }) {
 
     const productUrl = `/product/${data.id}`;
     const productName = `${data.name} - ${data.currentColor}`;
+    const currentDate = new Date().toISOString().split('T')[0];
+    const brandName = "Rybka Space";
     
     return (
         <div className={styles.container} itemScope itemType="https://schema.org/Product">
@@ -71,14 +73,52 @@ export default function ProductSlider({ data }) {
 
             <meta itemProp="name" content={productName} />
             <meta itemProp="description" content={data.info?.desc || ''} />
-            <meta itemProp="price" content={data.price} />
-            <meta itemProp="priceCurrency" content="UAH" />
-            {data.oldPrice && (
-                <>
-                    <meta itemProp="priceValidUntil" content={new Date().toISOString().split('T')[0]} />
-                    <meta itemProp="price" content={data.oldPrice} />
-                </>
-            )}
+            <meta itemProp="sku" content={`RS-${data.id}`} />
+            <meta itemProp="productID" content={`RS-${data.id}`} />
+            <meta itemProp="category" content="Жіночі спортивні костюми" />
+            <meta itemProp="url" content={`https://rybkaspace.com${productUrl}`} />
+            
+            <div itemProp="brand" itemScope itemType="https://schema.org/Brand">
+                <meta itemProp="name" content={brandName} />
+            </div>
+
+            <div itemProp="offers" itemScope itemType="https://schema.org/Offer">
+                <meta itemProp="price" content={data.price} />
+                <meta itemProp="priceCurrency" content="UAH" />
+                <meta itemProp="availability" content={data.disabled ? "https://schema.org/OutOfStock" : "https://schema.org/InStock"} />
+                <meta itemProp="url" content={`https://rybkaspace.com${productUrl}`} />
+                <meta itemProp="priceValidUntil" content={currentDate} />
+                {data.oldPrice && (
+                    <div itemProp="priceSpecification" itemScope itemType="https://schema.org/PriceSpecification">
+                        <meta itemProp="price" content={data.oldPrice} />
+                        <meta itemProp="priceCurrency" content="UAH" />
+                    </div>
+                )}
+            </div>
+
+            <div itemProp="additionalProperty" itemScope itemType="https://schema.org/PropertyValue">
+                <meta itemProp="name" content="Колір" />
+                <meta itemProp="value" content={data.currentColor} />
+            </div>
+
+            {data.grid?.sizes?.map((size) => (
+                <div key={size} itemProp="additionalProperty" itemScope itemType="https://schema.org/PropertyValue">
+                    <meta itemProp="name" content="Розмір" />
+                    <meta itemProp="value" content={size} />
+                </div>
+            ))}
+
+            <div itemProp="manufacturer" itemScope itemType="https://schema.org/Organization">
+                <meta itemProp="name" content={brandName} />
+                <meta itemProp="address" content="Україна, м. Ізмаїл, проспект Миру, 36" />
+            </div>
+
+            {data.info?.material?.map((material, index) => (
+                <div key={index} itemProp="material" itemScope itemType="https://schema.org/PropertyValue">
+                    <meta itemProp="name" content="Матеріал" />
+                    <meta itemProp="value" content={material} />
+                </div>
+            ))}
 
             <button ref={navigationPrevRef} className={`${styles.navButton} ${styles.prevButton}`}>
                 <ChevronLeft />
