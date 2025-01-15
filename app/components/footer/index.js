d7e004cb3af4634aba3fc3f43315ed6c178a70e6
v.kiriiak <valentyn.kiriiak@playtech.com>
2025-01-15 14:28:33 +0200

    reviews

--- app/components/footer/index.js
+++ app/components/footer/index.js
@@ -1,46 +1,49 @@
 import Image from "next/image";
 import styles from "./footer.module.scss";
+import {useTranslations} from 'next-intl';
 
 const Footer = () => {
+    const t = useTranslations('footer');
+    
     return (
         <footer className={styles.container} role="contentinfo">
             <div className={styles.copyright}>
                 2024 Rybka.space
             </div>
-            <nav className={styles.social} aria-label="Соціальні мережі">
+            <nav className={styles.social} aria-label={t('socialMedia')}>
                 <a href="https://www.instagram.com/rybka.space?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
                    className={styles.socialItem}
                    target="_blank" rel="noopener noreferrer"
-                   aria-label="Наш Instagram">
+                   aria-label={t('ourInstagram')}>
                     <Image
                         src="/images/i.svg"
                         fill
-                        alt="Наш Instagram - жіночі спортивні костюми Rybka Space"
+                        alt={t('instagramAlt')}
                     />
                 </a>
                 <a href="https://www.facebook.com/profile.php?id=61566339753971"
                    className={styles.socialItem}
                    target="_blank" rel="noopener noreferrer"
-                   aria-label="Наш Facebook">
+                   aria-label={t('ourFacebook')}>
                     <Image
                         src="/images/f.svg"
                         fill
-                        alt="Наш Facebook - жіночі спортивні костюми Rybka Space"
+                        alt={t('facebookAlt')}
                     />
                 </a>
                 <a href="https://www.tiktok.com/@rybka.space?is_from_webapp=1&sender_device=pc"
                    className={styles.socialItem}
                    target="_blank" rel="noopener noreferrer"
-                   aria-label="Наш TikTok">
+                   aria-label={t('ourTiktok')}>
                     <Image
                         src="/images/t.svg"
                         fill
-                        alt="Наш TikTok - жіночі спортивні костюми Rybka Space"
+                        alt={t('tiktokAlt')}
                     />
                 </a>
             </nav>
             <address className={styles.address}>
-                наша адреса: Україна, м. Ізмаїл, проспект Миру, 36.
+                {t('address')}
             </address>
         </footer>
     );
