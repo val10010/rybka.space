a0846de24da86f694bed6f89f8040c02e23e1254
v.kiriiak <valentyn.kiriiak@playtech.com>
2025-02-10 13:31:15 +0200

    image

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -105,8 +105,8 @@ export default async function LocaleLayout({children, params: {locale}}) {
                         async
                         defer
                     />
-                    <Script 
-                        id="google-analytics" 
+                    <Script
+                        id="google-analytics"
                         strategy="lazyOnload"
                         defer
                     >
@@ -144,6 +144,16 @@ export default async function LocaleLayout({children, params: {locale}}) {
                         }
                     `}
                     </Script>
+                    <Script defer id="hotjar" strategy="lazyOnload">{`
+                        (function(h,o,t,j,a,r){
+                            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                            h._hjSettings={hjid:5256627,hjsv:6};
+                            a=o.getElementsByTagName('head')[0];
+                            r=o.createElement('script');r.async=1;
+                            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                            a.appendChild(r);
+                        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                    `}</Script>
                     <SchemaOrganization />
                 </NextIntlClientProvider>
             </body>
