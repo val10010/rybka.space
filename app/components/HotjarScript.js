1f6075ed6ccbb5ea36d30115790560384b34ff8c
v.kiriiak <valentyn.kiriiak@playtech.com>
2025-02-10 14:45:27 +0200

    anal

--- app/components/HotjarScript.js
+++ app/components/HotjarScript.js
@@ -1,12 +1,17 @@
 'use client';
 
 export default function loadHotjar() {
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5256627,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    // Проверяем, что мы не в режиме разработки
+    console.log(process.env.NODE_ENV)
+    if (process.env.NODE_ENV !== 'development') {
+
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:5256627,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    }
 }
